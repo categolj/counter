a8c3105a4c6fa71aec463f7e10cc86de73ee0cca
Toshiaki Maki <tmaki@vmware.com>
2023-06-19 17:32:56 +0900

    Copy wasm assets to fly/app directly

--- fly/deploy.sh
+++ fly/deploy.sh
@@ -3,10 +3,6 @@ set -e -o pipefail
 DIR=$(dirname $0)
 set -x
 pushd ${DIR} > /dev/null
-mkdir -p app
-rm -rf app/counter.*
-cp ../target/wasm/counter.wasm ./app
-cp ../target/wasm/counter.toml ./app
 
 APP=wws
 # flyctl apps create --name ${APP} --machines
